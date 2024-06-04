4f39a01444395f0f3e10809c9adfb91b025c851a
Deftu <deftudev@gmail.com>
2024-06-04 17:10:55 +0200

    Build updates + more version support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,29 @@
 plugins {
-    id("xyz.deftu.gradle.multiversion-root")
+    java
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    `maven-publish`
+    id("dev.deftu.gradle.multiversion")
+    id("dev.deftu.gradle.tools")
+    id("dev.deftu.gradle.tools.resources")
+    id("dev.deftu.gradle.tools.blossom")
+    id("dev.deftu.gradle.tools.minecraft.loom")
+    id("dev.deftu.gradle.tools.minecraft.releases")
 }
 
-preprocess {
-    val fabric11902 = createNode("1.19.2-fabric", 11902, "yarn")
+toolkit.useDevAuth()
+
+repositories {
+    maven("https://maven.terraformersmc.com/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.fabricApiVersion}")
+    modImplementation(mcData.modMenuDependency)
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.7.4+kotlin.1.6.21")
+
+    modImplementation("dev.deftu:DeftuLib-${mcData.versionStr}-${mcData.loader.name}:1.8.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,22 +25,28 @@ pluginManagement {
         kotlin("jvm") version(kotlin)
         kotlin("plugin.serialization") version(kotlin)
 
-        val dgt = "1.5.1"
-        id("xyz.deftu.gradle.multiversion-root") version(dgt)
+        val dgt = "1.26.0"
+        id("dev.deftu.gradle.multiversion-root") version(dgt)
     }
 }
 
 val projectName: String = extra["mod.name"]?.toString()
     ?: throw MissingPropertyException("mod.name has not been set.")
 rootProject.name = projectName
-rootProject.buildFileName = "build.gradle.kts"
+rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
-    "1.19.2-fabric"
+    "1.18.2-fabric",
+    "1.19.2-fabric",
+    "1.19.4-fabric",
+    "1.20.1-fabric",
+    "1.20.2-fabric",
+    "1.20.4-fabric",
+    "1.20.6-fabric"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
         projectDir = file("versions/$version")
-        buildFileName = "../../version.gradle.kts"
+        buildFileName = "../../build.gradle.kts"
     }
 }
