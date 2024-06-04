4f8db6aa09e5a63d22b9ea7a2e2e84d6c6ad944b
Deftu <deftudev@gmail.com>
2024-06-04 17:23:27 +0200

    Add automatic publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import com.modrinth.minotaur.dependencies.DependencyType
+import com.modrinth.minotaur.dependencies.ModDependency
+import dev.deftu.gradle.tools.minecraft.CurseRelation
+import dev.deftu.gradle.tools.minecraft.CurseRelationType
+
 plugins {
     java
     kotlin("jvm")
@@ -26,4 +31,26 @@ dependencies {
     modImplementation("net.fabricmc:fabric-language-kotlin:1.7.4+kotlin.1.6.21")
 
     modImplementation("dev.deftu:DeftuLib-${mcData.versionStr}-${mcData.loader.name}:1.8.0")
+}
+
+toolkitReleases {
+    modrinth {
+        projectId.set("GbTwnB48")
+        dependencies.set(listOf(
+            ModDependency("P7dR8mSH", DependencyType.REQUIRED),                     // Fabric API
+            ModDependency("Ha28R6CL", DependencyType.REQUIRED),                     // Fabric Language Kotlin
+            ModDependency("mOgUt4GM", DependencyType.OPTIONAL),                     // Mod Menu
+            ModDependency("WfhjX9sQ", DependencyType.REQUIRED)                      // DeftuLib
+        ))
+    }
+
+    curseforge {
+        projectId.set("828667")
+        relations.set(listOf(
+            CurseRelation("fabric-api", CurseRelationType.REQUIRED),                // Fabric API
+            CurseRelation("fabric-language-kotlin", CurseRelationType.REQUIRED),    // Fabric Language Kotlin
+            CurseRelation("modmenu", CurseRelationType.OPTIONAL),                   // Mod Menu
+            CurseRelation("deftulib", CurseRelationType.REQUIRED)                   // DeftuLib
+        ))
+    }
 }
